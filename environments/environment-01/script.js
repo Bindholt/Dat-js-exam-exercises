75e661ffefd7ef321d87cd23a60c677a4cddb10f
Bindholt <Cbindholt@gmail.com>
2023-06-12 15:53:46 +0200

    Update script.js

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,40 @@
 "use strict";
 
+window.addEventListener("load", init);
+
+async function init() {
+    let users = await getJSONData();
+    console.log(users);
+    users = filterUsersByAdmins(users);
+    showUserList(users);
+}
+
+function filterUsersByAdmins(users) {
+    return users.filter(user => user.role === "admin");
+}
+
+async function getJSONData() {
+    const response = await fetch("users.json");
+
+    const usersData = await response.json();
+	const usersArray = [];
+	for (const key in usersData) {
+		const data = usersData[key];
+		data["id"] = key;
+		usersArray.push(data);
+    }
+
+    return usersArray;
+}
+
+function showUserList(users) {
+
+    for (const user in users) {
+        const userHTML = /* html */ `
+            <li>${users[user].name} ${(users[user].active) ? "(aktiv)" : "(inaktiv)" }</li>
+        `;
+
+        document.querySelector("#userlist").insertAdjacentHTML("afterbegin", userHTML);
+    }
+
+}
