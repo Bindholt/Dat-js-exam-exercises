1363437b61608ab9d496248a54c3cf2a3def1286
Bindholt <Cbindholt@gmail.com>
2023-06-12 16:35:58 +0200

    Update script.js

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,47 @@
 "use strict";
 
+const users = [];
+
+window.addEventListener("load", init); 
+
+async function init() {
+    await setGlobalUsers();
+    console.log(users);
+    displayActiveUsersHTML();
+}
+
+async function setGlobalUsers() {
+    const response = await fetch("users.json");
+    const usersData = await response.json();
+
+    for (const user in usersData) {
+        const data = usersData[user];
+		data["id"] = user;
+		users.push(data);
+    }
+}
+
+function displayActiveUsersHTML() {
+    for (const user in users) {
+        displayActiveUserHTML(user);
+    }
+}
+
+function displayActiveUserHTML(user) {
+    if (users[user].active) {
+        const userHTML = /* html */ `
+                <li>${users[user].name}</li>
+            `;
+        document.querySelector("#userlist").insertAdjacentHTML("afterbegin", userHTML);
+    }
+}
+
+function addUser(name, active, role) {
+    users.push({
+        name,
+        active,
+        role
+    });
+
+    displayActiveUserHTML(users.length - 1);
+}
