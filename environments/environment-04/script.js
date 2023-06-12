2a90a361e60d983fe27e6ef8b43784d973dfd94a
Bindholt <Cbindholt@gmail.com>
2023-06-12 20:50:34 +0200

    Update script.js

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+window.addEventListener("load", init);
+
+async function init() {
+    let teachers = [];
+    teachers = await importTeachers();
+    console.log(teachers);
+}
+
+async function importTeachers() {
+    const response = await fetch("teachers.js");
+    const teachersData = await response.json();
+    const teachersArray = [];
+
+    for (const teacher in teachersData) {
+        const data = teachers[teacher];
+		data["id"] = teacher;
+		teachersArray.push(data);
+    }
+    
+    return teachersArray;
+}
