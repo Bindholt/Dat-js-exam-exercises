d03be183785c1809d2597f6304970d4c9b3f6fb7
Bindholt <Cbindholt@gmail.com>
2023-06-12 16:23:58 +0200

    Update script.js

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,38 @@
 "use strict";
 
+window.addEventListener("load", init);
+
+const animals = []; 
+
+function init() {
+    addEventListeners();
+}
+
+function addEventListeners() {
+    document.querySelector("#create-form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        addAnimal(event.target["animal-name"].value, event.target["animal-type"].value, event.target["animal-age"].value);
+        document.querySelector("#create-form").reset();
+    });
+}
+
+function addAnimal(name, type, age) {
+    animals.push({
+        name,
+        type,
+        age,
+    });
+    setAnimalHTML(animals[animals.length -1]);
+}
+
+function setAnimalHTML(animal) {
+    console.log(animal.name);
+    const animalHTML = /* html */ `
+        <tr>
+            <td>${animal.name}</td>
+            <td>${animal.type}</td>
+            <td>${animal.age}</td>
+        </tr>
+    `;
+    document.querySelector("#list-container").querySelector("tbody").insertAdjacentHTML("beforeend", animalHTML);
+}
