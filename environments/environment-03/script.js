74ddc0b3881e687b203f72e4ba7f4b5ebaeef200
Bindholt <Cbindholt@gmail.com>
2023-06-12 20:19:36 +0200

    Update script.js

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,1 +1,54 @@
 "use strict";
+
+window.addEventListener("load", init);
+
+
+const products = [];
+function init() {
+
+    products.push(createProductObject("iPhone 4", 299, false));
+    products.push(createProductObject("iPhone 7", 1299, true));
+    products.push(createProductObject("iPhone X", 2299, true));
+    displayProductList();
+
+    addEventListeners();
+}   
+
+function addEventListeners() {
+    document.querySelector("#form-container").addEventListener("submit", (event) => {
+        event.preventDefault();
+        products.push({
+            name: event.target["name"].value,
+            price: Number(event.target["price"].value),
+            inStock: event.target["inStock"].checked,
+        });
+        displayProductList();
+    });
+}
+
+function createProductObject(name, price, inStock) {
+    return {
+        name,
+        price,
+        inStock
+    }
+}
+
+function displayProductList() {
+    sortProductsByStock();
+    document.querySelector("#list-container").innerHTML = "";
+    for (const product in products) {
+        displayProduct(products[product]);
+    }
+}
+
+function sortProductsByStock() {
+    products.sort((a,b) => b.inStock - a.inStock);
+}
+
+function displayProduct(product) {
+    const productHTML = /* html */ `
+        <p>${product.name} ${product.price},-</p>
+    `
+    document.querySelector("#list-container").insertAdjacentHTML("beforeend", productHTML);
+}
